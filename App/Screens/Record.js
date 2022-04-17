db3e772786cb0a2ddc8cb86b7f009641bbd29a01
Hasan Aasim <k181059@nu.edu.pk>
2022-04-17 13:49:49 +0500

    First Commit

--- App/Screens/Record.js
+++ App/Screens/Record.js
@@ -1,20 +1,99 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Button } from "react-native";
 import { Camera } from "expo-camera";
-import { useState, useEffect } from "react";
 import Icon from "react-native-vector-icons/FontAwesome";
+import { Video } from "expo-av";
 
 export default function App({ navigation: { navigate } }) {
-  const [hasPermission, setHasPermission] = useState(null);
+  const [hasAudioPermission, setHasAudioPermission] = useState(null);
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [record, setRecord] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === "granted");
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === "granted");
+
+      const audioStatus = await Camera.requestMicrophonePermissionsAsync();
+      setHasAudioPermission(audioStatus.status === "granted");
     })();
   }, []);
 
+  const takeVideo = async () => {
+    if (camera) {
+      const data = await camera.recordAsync({
+        maxDuration: 10,
+      });
+      setRecord(data.uri);
+      console.log(data.uri); // most prob the way to send the video to flask Idk?
+    }
+  };
+
+  const stopVideo = async () => {
+    camera.stopRecording();
+  };
+
+  if (hasCameraPermission === null || hasAudioPermission === null) {
+    return <View />;
+  }
+
+  if (hasCameraPermission === false || hasAudioPermission === false) {
+    return <Text> No Access To Camera</Text>;
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={styles.cameraContainer}>
+        <Camera
+          ref={(ref) => setCamera(ref)}
+          style={styles.fixedRatio}
+          type={type}
+          ratio={"4:3"}
+        />
+      </View>
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: record,
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status.isPlaying ? "Pause" : "Play"}
+          onPress={() =>
+            status.isPlaying
+              ? video.current.pauseAsync()
+              : video.current.playAsync()
+          }
+        />
+      </View>
+      <Button
+        title="Flep"
+        onPress={() => {
+          setType(
+            type === camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+        }}
+      />
+
+      <Button title="Take Video" onPress={() => takeVideo()} />
+      <Button title="Stop Video" onPress={() => stopVideo()} />
+    </View>
+  );
+}
+
+/*
   if (hasPermission === null) {
     return <View />;
   }
@@ -50,6 +129,7 @@ export default function App({ navigation: { navigate } }) {
     </View>
   );
 }
+*/
 
 const styles = StyleSheet.create({
   container: {
@@ -65,5 +145,23 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 25,
     paddingLeft: 15,
+  },
+  cameraContainer: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  fixedRatio: {
+    flex: 1,
+    aspectRatio: 1,
+  },
+  video: {
+    alignSelf: "center",
+    width: 350,
+    height: 220,
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
